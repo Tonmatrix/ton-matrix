c2ade5f7ae1da25b1f0c9592d48de5cbd58a80b0
Tonmatrix <vtuapp08@gmail.com>
2026-05-17 16:33:10 +0100

    Add files via upload
    just updating for v3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4013361019"
+    serviceWorkerVersion: "2421049656"
   }
 });
